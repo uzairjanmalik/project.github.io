ed13c31751773a0960cfdcd6e07463fac20e7880
uzairjanmalik <muhammaduzair1528@gmail.com>
2024-05-08 13:49:58 +0500

    project

---
+++ index.js
@@ -0,0 +1,8 @@
+//toggle menu button
+
+function togglemenu(){
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.nav');
+    menu.classList.toggle('active');
+    nav.classList.toggle('active');         
+}
